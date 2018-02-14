043ea1441a9e43ed06912028046943b61e4d8521
ThunyasitP <36465802+ThunyasitP@users.noreply.github.com>
2018-02-14 15:47:37 +0700

    ff
    rd

---
+++ update.php
@@ -0,0 +1,13 @@
+<?php
+
+	//GET SQLITE
+	$dir = 'sqlite:/chatbot.sqlite';
+	$dbh  = new PDO($dir) or die("cannot open the database");
+	$query =  "SELECT * FROM talk'";
+	foreach ($dbh->query($query) as $row)
+	{
+	    echo $row[0];
+	} 
+
+
+?>
