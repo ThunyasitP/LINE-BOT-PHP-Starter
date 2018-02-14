7392d667e7245cab08a33d3fbf45ef2d66470ac0
ThunyasitP <36465802+ThunyasitP@users.noreply.github.com>
2018-02-14 15:49:16 +0700

    ddd
    ddsd

--- update.php
+++ update.php
@@ -1,9 +1,9 @@
 <?php
 
 	//GET SQLITE
-	$dir = 'sqlite:/chatbot.sqlite';
+	$dir = 'sqlite:chatbot.sqlite';
 	$dbh  = new PDO($dir) or die("cannot open the database");
-	$query =  "SELECT * FROM talk'";
+	$query =  "SELECT * FROM talk";
 	foreach ($dbh->query($query) as $row)
 	{
 	    echo $row[0];
