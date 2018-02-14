ed197063f3f9b232cd26ecfbfbf5f3d4cae3a679
ThunyasitP <36465802+ThunyasitP@users.noreply.github.com>
2018-02-14 14:34:13 +0700

    sdsd
    sdsd

--- chat.php
+++ chat.php
@@ -9,24 +9,13 @@ $events = json_decode($content, true);
 
 $messageWelcome = 'ขอบคุณที่ส่งข้อความถึงเรา .. TVIClaim ยินดีบริการ เราพร้อมอยู่เคียงข้างและดูแลคุณตลอด 24 ชม. กรุณาเลือกบริการที่ท่านต้องการติดต่อ';
 
-$messageConfrim = array(
-				
-	'type' => 'template',
-	'altText' => 'QA',
-	'template' =>  array(
-	      'type' => 'confirm',
-	      'text' => 'Are you sure?',
-	      'actions' => array(
-	          array(
-	            'type' => 'message',
-	            'label' => 'Yes',
-	            'text' => 'yes'
-	          )
-	      )
-  )
-  
-);
-
+		$messageTopic1	= 'บริการแจ้งเคลม TVIClaim ยินดีดูแลลูกค้าและเคียงข้างตลอด 24 ชม.';
+		$messageTopic2	= 'บริการแจ้งซ่อม TVIClaim ยินดีดูแลลูกค้าและเคียงข้างตลอด 24 ชม.';
+$messageCallname 	= 'กรุณาระบุ ชื่อ และนามสกุล';
+$messagePlatNumber 	= 'กรุณาระบุ หมายเลขทะเบียนรถ';
+$messageTel 		= 'กรุณาระบุ เบอร์โทรศัพท์';
+$messageLocation 	= 'กรุณาระบุ ระบุตำแหน่ง';
+$messageThank	= 'ขอบคุณสำหรับข้อมูลของท่านค่ะ';
 
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -39,73 +28,82 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				
-				
-					'type' => 'template',
-					'altText' => 'QA',
-					'template' =>	array(
-				
-							'type' => 'template',
-							'altText' => 'QA',
-							'template' =>  array(
-							      'type' => 'confirm',
-							      'text' => 'Are you sure?',
-							      'actions' => array(
-							          array(
-							            'type' => 'message',
-							            'label' => 'Yes',
-							            'text' => 'yes'
-							          )
-							      )
-						 	)
-						  
-					)
-			];
+	
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => array(
-					
-						array(
 
-							'type' => 'text',
-							'text' => $messageWelcome
 
-						),
-						array(
-					
-								'type' => 'template',
-								'altText' => 'ท่านต้องการติดต่อ?',
-								'template' =>	array(
-							
-									
-										      'type' => 'confirm',
-										      'text' => 'ท่านต้องการติดต่อ?',
-										      'actions' => array(
-										          array(
-										            'type' => 'message',
-										            'label' => '(1) แจ้งเหตุ',
-										            'text' => '1'
-										          ),
-										          array(
-										            'type' => 'message',
-										            'label' => '(2) แจ้งซ่อม',
-										            'text' => '2'
-										          )										          
 
-										      )
-									 	
-									  
-								)
 
+			//START STEP (1)
+			if($text == '1' || $text == '2')
+			{
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => array(
+						
+							array(
+
+								'type' => 'text',
+								'text' => replyTopic($text)
+
+							), 
+						
+							array(
+
+								'type' => 'text',
+								'text' => $messageCallname
+
+							), 							
+						)
+				];
+
+			}
+			else
+			{
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => array(
+						
+							array(
+
+								'type' => 'text',
+								'text' => $messageWelcome
+
+							),
+							array(
+						
+									'type' => 'template',
+									'altText' => 'ท่านต้องการติดต่อ?',
+									'template' =>	array(
+								
+										
+											      'type' => 'confirm',
+											      'text' => 'ท่านต้องการติดต่อ?',
+											      'actions' => array(
+											          array(
+											            'type' => 'message',
+											            'label' => '(1) แจ้งเคลม',
+											            'text' => '1'
+											          ),
+											          array(
+											            'type' => 'message',
+											            'label' => '(2) แจ้งซ่อม',
+											            'text' => '2'
+											          )										          
+
+											      )
+										 	
+										  
+									)
+
+							)
+						
 						)
 					
-					)
-				
-			];
+				];
+			}
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
@@ -124,10 +122,50 @@ if (!is_null($events['events'])) {
 
 
 		}
+		else if ($event['type'] == 'message' && $event['message']['type'] == 'location') {
+
+
+
+		}
 	}
 
 	
 }
 echo "OK";
+function replyTopic( $message )
+{
+
+		$messageCallname 	= 'กรุณาระบุ ชื่อ และนามสกุล';
+		$messagePlatNumber 	= 'กรุณาระบุ หมายเลขทะเบียนรถ';
+		$messageTel 		= 'กรุณาระบุ เบอร์โทรศัพท์';
+		$messageLocation 	= 'กรุณาระบุ ระบุตำแหน่ง';
+	
+		$messageThank	= 'ขอบคุณสำหรับข้อมูลของท่านค่ะ';
+
+		$messageTopic1	= 'บริการแจ้งเคลม TVIClaim ยินดีดูแลลูกค้าและเคียงข้างตลอด 24 ชม.';
+		$messageTopic2	= 'บริการแจ้งซ่อม TVIClaim ยินดีดูแลลูกค้าและเคียงข้างตลอด 24 ชม.';
+
+
+
+}
+
+function replyMessage( $message )
+{
+
+		$messageCallname 	= 'กรุณาระบุ ชื่อ และนามสกุล';
+		$messagePlatNumber 	= 'กรุณาระบุ หมายเลขทะเบียนรถ';
+		$messageTel 		= 'กรุณาระบุ เบอร์โทรศัพท์';
+		$messageLocation 	= 'กรุณาระบุ ระบุตำแหน่ง';
+		$messageThank		= 'ขอบคุณสำหรับข้อมูลของท่านค่ะ';
+		if( $message == 'Thunyasit Pholprasit')
+		{
+			return $messagePlatNumber;
+		}
+		else if( $message == '4435')
+		{
+			return $messageLocation;
+		}
+
+}
 
 ?>
